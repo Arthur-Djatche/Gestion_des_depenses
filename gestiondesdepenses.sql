2511721d0eb2ee3b2bb5a7054da96c4cab18c9ab
Carelle <anniemasso07@gmail.com>
2025-11-02 07:23:43 +0100

    Ajout des fichiers d inscription et ajout de la colonne mot_de_passe au fichier sql de la bd

--- gestiondesdepenses.sql
+++ gestiondesdepenses.sql
@@ -64,6 +64,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
   `email` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `mot_de_passe` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
